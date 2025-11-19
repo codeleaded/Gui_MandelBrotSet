3528bd219b0a03d8cdb8a5694b02f144b651d2bb
C-Reaper <gnu.c.reaper@gmail.com>
2025-11-19 19:32:20 +0100

    updated

--- src/Main.h
+++ src/Main.h
@@ -1,11 +1,18 @@
+#include <math.h>
+//#include "C:/Wichtig/System/Static/Library/WindowEngine1.0.h"
+//#include "C:/Wichtig/System/Static/Library/Victor2.h"
+//#include "C:/Wichtig/System/Static/Library/Vdctor2.h"
+//#include "C:/Wichtig/System/Static/Library/Complex2.h"
+//#include "C:/Wichtig/System/Static/Library/TransformedView.h"
+//#include "C:/Wichtig/System/Static/Container/Vector.h"
+
 #include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
-#include "/home/codeleaded/System/Static/Container/Vector.h"
 #include "/home/codeleaded/System/Static/Library/Victor2.h"
 #include "/home/codeleaded/System/Static/Library/Vdctor2.h"
 #include "/home/codeleaded/System/Static/Library/Complex2.h"
 #include "/home/codeleaded/System/Static/Library/TransformedView.h"
 #include "/home/codeleaded/System/Static/Library/Thread.h"
-#include "/home/codeleaded/System/Static/Library/ThreadPool.h"
+#include "/home/codeleaded/System/Static/Container/Vector.h"
 
 #define nMaxThreads 32
 
@@ -129,13 +136,14 @@ void CreateFractalIntrinsics(const Vic2* pix_tl, const Vic2* pix_br, const Vdc2*
 	_x_jump = _mm256_set1_pd(x_scale * 4);
 	_x_pos_offsets = _mm256_set_pd(0, 1, 2, 3);
 	_x_pos_offsets = _mm256_mul_pd(_x_pos_offsets, _x_scale);
-	
-	for (y = pix_tl->y; y < pix_br->y; y++){
+	for (y = pix_tl->y; y < pix_br->y; y++)
+	{
+		// Reset x_position
 		_a = _mm256_set1_pd(frac_tl->x);
 		_x_pos = _mm256_add_pd(_a, _x_pos_offsets);
 		_ci = _mm256_set1_pd(y_pos);
-		
-		for (x = pix_tl->x; x < pix_br->x; x += 4){
+		for (x = pix_tl->x; x < pix_br->x; x += 4)
+		{
 			_cr = _x_pos;
 			_zr = _mm256_setzero_pd();
 			_zi = _mm256_setzero_pd();
@@ -172,6 +180,7 @@ void CreateFractalIntrinsics(const Vic2* pix_tl, const Vic2* pix_br, const Vdc2*
 }
 
 
+#ifdef __linux__
 
 typedef struct Element{
 	Thread hThread;
@@ -184,11 +193,24 @@ typedef struct Element{
 	int i;
 } Element;
 
+Element Threads[nMaxThreads];
+
 void* Thread_Func(void* arg){
 	Element* e = arg;
 	CreateFractalIntrinsics(&e->a,&e->b,&e->c,&e->d,e->i);
 	return NULL;
 }
+
+void* Thread_Pool_Func(void* arg){
+	Element* e = arg;
+	while(e->bAlive){
+		while(e->bLocked){ Thread_Sleep_U(500); }
+		CreateFractalIntrinsics(&e->a,&e->b,&e->c,&e->d,e->i);
+		e->bLocked = 1;
+	}
+	return NULL;
+}
+
 void CreateFractalThreads(const Vic2* pix_tl, const Vic2* pix_br, const Vdc2* frac_tl, const Vdc2* frac_br, const int iterations){
 	int nSectionWidth = (pix_br->x - pix_tl->x) / nMaxThreads;
 	double dFractalWidth = (frac_br->x - frac_tl->x) / (double)nMaxThreads;
@@ -209,51 +231,179 @@ void CreateFractalThreads(const Vic2* pix_tl, const Vic2* pix_br, const Vdc2* fr
 	}
 }
 
+void InitThreadPool(){
+	for (size_t i = 0; i < nMaxThreads; i++){
+		if(nMode==5){
+			Threads[i].bLocked = 1;
+			Threads[i].bAlive = 1;
+			Threads[i].hThread = Thread_New(NULL,Thread_Pool_Func,&Threads[i]);
+			Thread_Start(&Threads[i].hThread);
+		}else{
+			Threads[i].bLocked = 1;
+			Threads[i].bAlive = 0;
+			Threads[i].hThread.h;
+		}
+	}
+}
 
-typedef struct PoolElement{
+void CreateFractalThreadPool(const Vic2* pix_tl, const Vic2* pix_br, const Vdc2* frac_tl, const Vdc2* frac_br, const int iterations){
+	int nSectionWidth = (pix_br->x - pix_tl->x) / nMaxThreads;
+	double dFractalWidth = (frac_br->x - frac_tl->x) / (double)nMaxThreads;
+	
+	for (size_t i = 0; i < nMaxThreads; i++){
+		Threads[i].a = (Vic2){ pix_tl->x + nSectionWidth * (i), pix_tl->y };
+		Threads[i].b = (Vic2){ pix_tl->x + nSectionWidth * (i + 1), pix_br->y };
+		Threads[i].c = (Vdc2){ frac_tl->x + dFractalWidth * (double)(i), frac_tl->y };
+		Threads[i].d = (Vdc2){ frac_tl->x + dFractalWidth * (double)(i + 1), frac_br->y };
+		Threads[i].i = (int)iterations;
+
+		if(Threads[i].hThread.h==0UL){
+			Threads[i].bLocked = 1;
+			Threads[i].bAlive = 1;
+			Threads[i].hThread = Thread_New(NULL,Thread_Pool_Func,&Threads[i]);
+			Thread_Start(&Threads[i].hThread);
+		}
+	}
+	for (size_t i = 0; i < nMaxThreads; i++){
+		Threads[i].bLocked = 0;
+	}
+
+	Bool Running = 1;
+	while(Running){
+		for(size_t i = 0; i < nMaxThreads; i++){
+			if(!Threads[i].bLocked){
+				break;
+			}
+			if(i==nMaxThreads-1){
+				Running = 0;
+			}
+		}
+	}
+}
+
+void DeleteThreadPool(){
+	for (size_t i = 0; i < nMaxThreads; i++){
+		if(Threads[i].hThread.h){
+			Threads[i].bAlive = 0;
+			Threads[i].bLocked = 0;
+			Thread_Join(&Threads[i].hThread);
+			Threads[i].hThread.h = 0UL;
+		}
+	}
+}
+
+#elif _WIN64
+
+typedef struct Element{
+	HANDLE hThread;
+	Bool bLocked;
+	Bool bAlive;
 	Vic2 a;
 	Vic2 b;
 	Vdc2 c;
 	Vdc2 d;
 	int i;
-} PoolElement;
+} Element;
 
-ThreadPool pool;
+Element Threads[nMaxThreads];
 
-void* Thread_Pool_Func(ThreadWorker* tw){
-	PoolElement* e = (PoolElement*)tw->data;
+DWORD WINAPI Thread_Func(LPVOID lpParam){
+	Element* e = lpParam;
 	CreateFractalIntrinsics(&e->a,&e->b,&e->c,&e->d,e->i);
-	return NULL;
 }
+
+DWORD WINAPI Thread_Pool_Func(LPVOID lpParam){
+	Element* e = lpParam;
+	while(e->bAlive){
+		while(e->bLocked){ Thread_Sleep_M(1);(1); }
+		CreateFractalIntrinsics(&e->a,&e->b,&e->c,&e->d,e->i);
+		e->bLocked = 1;
+	}
+}
+
+void CreateFractalThreads(const Vic2* pix_tl, const Vic2* pix_br, const Vdc2* frac_tl, const Vdc2* frac_br, const int iterations){
+	int nSectionWidth = (pix_br->x - pix_tl->x) / nMaxThreads;
+	double dFractalWidth = (frac_br->x - frac_tl->x) / (double)nMaxThreads;
+	
+	Element Args[nMaxThreads];
+	HANDLE t[nMaxThreads];
+	for (size_t i = 0; i < nMaxThreads; i++){
+		Args[i].a = (Vic2){ pix_tl->x + nSectionWidth * (i), pix_tl->y };
+		Args[i].b = (Vic2){ pix_tl->x + nSectionWidth * (i + 1), pix_br->y };
+		Args[i].c = (Vdc2){ frac_tl->x + dFractalWidth * (double)(i), frac_tl->y };
+		Args[i].d = (Vdc2){ frac_tl->x + dFractalWidth * (double)(i + 1), frac_br->y };
+		Args[i].i = (int)iterations;
+		t[i] = CreateThread(NULL,0,Thread_Func,&Args[i],0,NULL);
+	}
+	
+	for (size_t i = 0; i < nMaxThreads; i++){
+		WaitForSingleObject(t[i],INFINITE);
+		CloseHandle(t[i]);
+	}
+}
+
+void InitThreadPool(){
+	for (size_t i = 0; i < nMaxThreads; i++){
+		if(nMode==5){
+			Threads[i].bLocked = 1;
+			Threads[i].bAlive = 1;
+			Threads[i].hThread = CreateThread(NULL,0,Thread_Pool_Func,&Threads[i],0,NULL);
+		}else{
+			Threads[i].bLocked = 1;
+			Threads[i].bAlive = 0;
+			Threads[i].hThread = NULL;
+		}
+	}
+}
+
 void CreateFractalThreadPool(const Vic2* pix_tl, const Vic2* pix_br, const Vdc2* frac_tl, const Vdc2* frac_br, const int iterations){
-	const int nSectionWidth = (pix_br->x - pix_tl->x) / pool.size;
-	const double dFractalWidth = (frac_br->x - frac_tl->x) / (double)pool.size;
-	for(int i = 0;i < pool.size;i++){
-		ThreadWorker* tw = (ThreadWorker*)Vector_Get(&pool,i);
-		PoolElement* pe = (PoolElement*)tw->data;
-		pe->a = (Vic2){ pix_tl->x + nSectionWidth * (i), pix_tl->y };
-		pe->b = (Vic2){ pix_tl->x + nSectionWidth * (i + 1), pix_br->y };
-		pe->c = (Vdc2){ frac_tl->x + dFractalWidth * (double)(i), frac_tl->y };
-		pe->d = (Vdc2){ frac_tl->x + dFractalWidth * (double)(i + 1), frac_br->y };
-		pe->i = (int)iterations;
+	int nSectionWidth = (pix_br->x - pix_tl->x) / nMaxThreads;
+	double dFractalWidth = (frac_br->x - frac_tl->x) / (double)nMaxThreads;
+	
+	for (size_t i = 0; i < nMaxThreads; i++){
+		Threads[i].a = (Vic2){ pix_tl->x + nSectionWidth * (i), pix_tl->y };
+		Threads[i].b = (Vic2){ pix_tl->x + nSectionWidth * (i + 1), pix_br->y };
+		Threads[i].c = (Vdc2){ frac_tl->x + dFractalWidth * (double)(i), frac_tl->y };
+		Threads[i].d = (Vdc2){ frac_tl->x + dFractalWidth * (double)(i + 1), frac_br->y };
+		Threads[i].i = (int)iterations;
+
+		if(Threads[i].hThread==NULL){
+			Threads[i].bLocked = 1;
+			Threads[i].bAlive = 1;
+			Threads[i].hThread = CreateThread(NULL,0,Thread_Pool_Func,&Threads[i],0,NULL);
+		}
+	}
+	for (size_t i = 0; i < nMaxThreads; i++){
+		Threads[i].bLocked = 0;
 	}
 
-	// const int nSectionHeight = (pix_br->y - pix_tl->y) / pool.size;
-	// const double dFractalHeight = (frac_br->y - frac_tl->y) / (double)pool.size;
-	// for(int i = 0;i < pool.size;i++){
-	// 	ThreadWorker* tw = (ThreadWorker*)Vector_Get(&pool,i);
-	// 	PoolElement* pe = (PoolElement*)tw->data;
-	// 	pe->a = (Vic2){ pix_tl->x,pix_tl->y + nSectionHeight * (i) };
-	// 	pe->b = (Vic2){ pix_br->x,pix_tl->y + nSectionHeight * (i + 1) };
-	// 	pe->c = (Vdc2){ frac_tl->x,frac_tl->y + dFractalHeight * (double)(i) };
-	// 	pe->d = (Vdc2){ frac_br->x,frac_tl->y + dFractalHeight * (double)(i + 1) };
-	// 	pe->i = (int)iterations;
-	// }
-
-	ThreadPool_Start(&pool);
-	ThreadPool_Join(&pool);
+	Bool Running = 1;
+	while(Running){
+		for(size_t i = 0; i < nMaxThreads; i++){
+			if(!Threads[i].bLocked){
+				break;
+			}
+			if(i==nMaxThreads-1){
+				Running = 0;
+			}
+		}
+	}
 }
 
+void DeleteThreadPool(){
+	for (size_t i = 0; i < nMaxThreads; i++){
+		if(Threads[i].hThread){
+			Threads[i].bAlive = 0;
+			Threads[i].bLocked = 0;
+			WaitForSingleObject(Threads[i].hThread,INFINITE);
+			//TerminateThread(Threads[i].hThread,0);
+			CloseHandle(Threads[i].hThread);
+			Threads[i].hThread = NULL;
+		}
+	}
+}
+
+#endif
 
 void Setup(AlxWindow* w){
 	tv = TransformedViewD_New((Vdc2){ GetWidth(),GetHeight() });
@@ -261,7 +411,8 @@ void Setup(AlxWindow* w){
 	pFractal = (int*)malloc((size_t)GetWidth() * (size_t)GetHeight() * sizeof(int));
 	memset(pFractal,0,(size_t)GetWidth() * (size_t)GetHeight() * sizeof(int));
 
-	pool = ThreadPool_New((void*(*)(void*))Thread_Pool_Func,sizeof(PoolElement),nMaxThreads);
+	//nMode = 5;
+	InitThreadPool();
 
 	ResizeAlxFont(16,16);
 }
@@ -289,23 +440,64 @@ void Update(AlxWindow* w){
 	
 	Timepoint tp1 = Time_Nano();
 	
-	switch (nMode){
-		case 0: CreateFractalBasic(&pix_tl,&pix_br,&frac_tl,&frac_br,nIterations); 			break;
-		case 1: CreateFractalPreCalculate(&pix_tl,&pix_br,&frac_tl,&frac_br,nIterations); 	break;
-		case 2: CreateFractalNoComplex2(&pix_tl,&pix_br,&frac_tl,&frac_br,nIterations); 	break;
-		case 3: CreateFractalIntrinsics(&pix_tl,&pix_br,&frac_tl,&frac_br,nIterations); 	break;
-		case 4: CreateFractalThreads(&pix_tl,&pix_br,&frac_tl,&frac_br,nIterations); 		break;
-		case 5: CreateFractalThreadPool(&pix_tl,&pix_br,&frac_tl,&frac_br,nIterations); 	break;
+	switch (nMode)
+	{
+	case 0: CreateFractalBasic(&pix_tl,&pix_br,&frac_tl,&frac_br,nIterations); 			break;
+	case 1: CreateFractalPreCalculate(&pix_tl,&pix_br,&frac_tl,&frac_br,nIterations); 	break;
+	case 2: CreateFractalNoComplex2(&pix_tl,&pix_br,&frac_tl,&frac_br,nIterations); 	break;
+	case 3: CreateFractalIntrinsics(&pix_tl,&pix_br,&frac_tl,&frac_br,nIterations); 	break;
+	case 4: CreateFractalThreads(&pix_tl,&pix_br,&frac_tl,&frac_br,nIterations); 		break;
+	case 5: CreateFractalThreadPool(&pix_tl,&pix_br,&frac_tl,&frac_br,nIterations); 	break;
 	}
 	
 	Timepoint tp2 = Time_Nano();
 	double elapsedTime = Time_Elapsed(tp1,tp2);
 
+	//for (int y = 0; y < GetHeight(); y++)
+	//{
+	//	for (int x = 0; x < GetWidth(); x++)
+	//	{
+	//		int i = pFractal[y * GetWidth() + x];
+	//		float n = (float)i;
+	//		float a = 0.1f;
+	//
+	//		Point_Render(WINDOW_STD_ARGS,(Vec2){ x,y },Pixel_toRGBA(
+	//			0.5f * sinf(a * n) + 0.5f,
+	//			0.5f * sinf(a * n + 2.094f) + 0.5f,
+	//			0.5f * sinf(a * n + 4.188f) + 0.5f,
+	//			1.0f
+	//		));
+	//	}
+	//}
+
 	for (int i = 0; i < GetWidth() * GetHeight(); i++){
 		int it = pFractal[i];
 		float n = (float)it;
 		float a = 0.05f;
-
+		//window.Buffer[i] = Pixel_toRGBA(
+		//	0.5f * sinf(a * n) + 0.5f,
+		//	0.5f * sinf(a * n + 2.094f) + 0.5f,
+		//	0.5f * sinf(a * n + 4.188f) + 0.5f,
+		//	1.0f
+		//);
+		//window.Buffer[i] = Pixel_toRGBA(
+		//	F32_LinPer(a * n,1.0f),
+		//	F32_LinPer(a * n,1.0f),
+		//	F32_LinPer(a * n,1.0f),
+		//	1.0f
+		//);
+		//window.Buffer[i] = Pixel_toRGBA(
+		//	0.5f * F32_LinPer(a * n,2*fPI) + 0.5f,
+		//	0.5f * F32_LinPer(a * n + 2.094f,2*fPI) + 0.5f,
+		//	0.5f * F32_LinPer(a * n + 4.188f,2*fPI) + 0.5f,
+		//	1.0f
+		//);
+		//window.Buffer[i] = Pixel_toRGBA(
+		//	0.5f * F32_Sin_Sq(a * n) + 0.5f,
+		//	0.5f * F32_Sin_Sq(a * n + 2.094f) + 0.5f,
+		//	0.5f * F32_Sin_Sq(a * n + 4.188f) + 0.5f,
+		//	1.0f
+		//);
 		window.Buffer[i] = Pixel_toRGBA(
 			0.5f * F32_Sin_Cb(a * n) + 0.5f,
 			0.5f * F32_Sin_Cb(a * n + 2.094f) + 0.5f,
@@ -314,15 +506,15 @@ void Update(AlxWindow* w){
 		);
 	}
 
-	switch (nMode){
-		case 0: RenderCStr("1) Naive Method",					0,0,WHITE); break;
-		case 1: RenderCStr("2) Precalculate Method",			0,0,WHITE); break;
-		case 2: RenderCStr("3) Hand-code Maths Method",			0,0,WHITE); break;
-		case 3: RenderCStr("4) Vector Extensions (AVX2) Method",0,0,WHITE); break;
-		case 4: RenderCStr("5) Threads Method",					0,0,WHITE); break;
-		case 5: RenderCStr("6) ThreadPool Method",				0,0,WHITE); break;
+	switch (nMode)
+	{
+	case 0: RenderCStr("1) Naive Method",					0,0,WHITE); break;
+	case 1: RenderCStr("2) Precalculate Method",			0,0,WHITE); break;
+	case 2: RenderCStr("3) Hand-code Maths Method",			0,0,WHITE); break;
+	case 3: RenderCStr("4) Vector Extensions (AVX2) Method",0,0,WHITE); break;
+	case 4: RenderCStr("5) Threads Method",					0,0,WHITE); break;
+	case 5: RenderCStr("6) ThreadPool Method",				0,0,WHITE); break;
 	}
-
 	String str = String_Format("| ES Taken: %fs - Iterations: %d |",elapsedTime,nIterations);
 	char* cstr = String_CStr(&str);
 	RenderCStr(cstr,0,30,WHITE);
@@ -330,7 +522,15 @@ void Update(AlxWindow* w){
 	String_Free(&str);
 }
 void Delete(AlxWindow* w){
-	ThreadPool_Free(&pool);
+    //for (int i = 0; i < nMaxThreads; i++){
+	//	workers[i].alive = 0;
+	//	workers[i].cvStart.notify_one();
+	//}
+    //
+	//for (int i = 0; i < nMaxThreads; i++)
+	//	workers[i].thread.join();
+
+	DeleteThreadPool();
 	free(pFractal);
 }
 void Resize(AlxWindow* w){
